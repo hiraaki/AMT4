dad2d7ff10f392d5639d3660a855d8ce75f86c66
MHI <mauricio_ishida@hotmail.com>
2019-06-27 22:04:18 -0300

    search and counting

--- Classificador.py
+++ Classificador.py
@@ -20,27 +20,37 @@ def sortSecond(val):
 data_frame = pd.read_csv("ct\\Base1.csv")
 x = data_frame.iloc[:, 1:17].values
 y = data_frame.iloc[:, 17].values
-
-BestK = -1
-BKAcur = -1
-for i in range(1, 100):
-    Kacur = 0
-    for j in range(5):
-        # divide a base em 50% para treino, 25% para teste e 25% para validação
-        train_x, aux_x, train_y, aux_y = train_test_split(x, y, test_size=0.5)
-        validation_x, test_x, validation_y, test_y = train_test_split(aux_x, aux_y, test_size=0.5)
-        nbrs = KNeighborsClassifier(n_neighbors=i, algorithm='auto')
-        nbrs.fit(train_x, train_y)
-        Kacur += accuracy(test_y, nbrs.predict(test_x))
-    Kacur = Kacur / 10
-    if Kacur > BKAcur:
-        BKAcur = Kacur
-        BestK = i
-print(BestK)
+# #
+# BestK = -1
+# BKAcur = -1
+# for i in range(1, 35):
+#     Kacur = 0
+#     for j in range(5):
+#         # divide a base em 50% para treino, 25% para teste e 25% para validação
+#         train_x, aux_x, train_y, aux_y = train_test_split(x, y, test_size=0.5)
+#         validation_x, test_x, validation_y, test_y = train_test_split(aux_x, aux_y, test_size=0.5)
+#         nbrs = KNeighborsClassifier(n_neighbors=i, algorithm='auto')
+#         nbrs.fit(train_x, train_y)
+#         Kacur += accuracy(test_y, nbrs.predict(test_x))
+#     Kacur = Kacur / 5
+#     if Kacur > BKAcur:
+#         BKAcur = Kacur
+#         BestK = i
+# print(BestK)
+# print(BKAcur)
+# print("--------------------")
+# # train_x, aux_x, train_y, aux_y = train_test_split(x, y, test_size=0.5)
+# # validation_x, test_x, validation_y, test_y = train_test_split(aux_x, aux_y, test_size=0.5)
+# # nbrs = KNeighborsClassifier(n_neighbors=1, algorithm='auto')
+# # nbrs.fit(train_x, train_y)
+# #
+# # print(nbrs.predict((test_x[5], test_x[1000])))
+# # print(test_y[500], test_y[1000])
+# #
 #
 # TreeAcu = []
 # TreeMed = []
-# for i in range(100):
+# for i in range(10):
 #     train_x, aux_x, train_y, aux_y = train_test_split(x, y, test_size=0.5)
 #     validation_x, test_x, validation_y, test_y = train_test_split(aux_x, aux_y, test_size=0.5)
 #     for j in range(1, 20):
@@ -53,69 +63,71 @@ print(BestK)
 #     for treeacu in TreeAcu:
 #         if treeacu[0] == i:
 #             acu += treeacu[1]
-#     TreeMed.append((i, acu/100))
-# bestMF = [0,-1]
+#     TreeMed.append((i, acu/10))
+# bestMF = [0, -1]
 # for treemed in TreeMed:
 #     if treemed[1] > bestMF[1]:
 #         bestMF = treemed
+#
 # print(bestMF)
+# print("--------------------")
+# #
+SVCacu = []
+SVCMed = []
+for i in range(10):
+    train_x, aux_x, train_y, aux_y = train_test_split(x, y, test_size=0.5)
+    validation_x, test_x, validation_y, test_y = train_test_split(aux_x, aux_y, test_size=0.5)
+    for j in np.arange(0.05, 20.0, 0.05):
+        svm = SVC(C=j, kernel='rbf', gamma='scale')
+        svm.fit(train_x, train_y)
+        SVCacu.append((j, 'rbf', accuracy(test_y, svm.predict(test_x))))
+    for j in np.arange(0.05, 20.0, 0.5):
+        svm = SVC(C=j, kernel='poly', gamma='scale')
+        svm.fit(train_x, train_y)
+        SVCacu.append((j, 'poly', accuracy(test_y, svm.predict(test_x))))
+for i in np.arange(0.01, 2.0, 0.02):
+    rbfacu = 0
+    polyacu = 0
+    for svcacu in SVCacu:
+        if svcacu[0] == i:
+            if svcacu[1] == 'poly':
+               polyacu += svcacu[2]
+            else:
+                rbfacu += svcacu[2]
+    SVCMed.append((i, 'poly', polyacu/10))
+    SVCMed.append((i, 'rbf', rbfacu/10))
+SVCBest = (0.0, '', -1)
+for svcmed in SVCMed:
+    if svcmed[2] > SVCBest[2]:
+        SVCBest = svcmed
+print(SVCBest)
 #
-# SVCacu = []
-# SVCMed = []
-# for i in range(10):
-#     train_x, aux_x, train_y, aux_y = train_test_split(x, y, test_size=0.5)
-#     validation_x, test_x, validation_y, test_y = train_test_split(aux_x, aux_y, test_size=0.5)
-#     for j in np.arange(0.01, 10.0, 0.5):
-#         svm = SVC(C=j, kernel='rbf', gamma='scale')
-#         svm.fit(train_x, train_y)
-#         SVCacu.append((j, 'rbf', accuracy(test_y, svm.predict(test_x))))
-#     for j in np.arange(0.01, 2.0, 0.5):
-#         svm = SVC(C=j, kernel='poly', gamma='scale')
-#         svm.fit(train_x, train_y)
-#         SVCacu.append((j, 'poly', accuracy(test_y, svm.predict(test_x))))
-# for i in np.arange(0.01, 2.0, 0.02):
-#     rbfacu = 0
-#     polyacu = 0
-#     for svcacu in SVCacu:
-#         if svcacu[0] == i:
-#             if svcacu[1] == 'poly':
-#                polyacu += svcacu[2]
-#             else:
-#                 rbfacu += svcacu[2]
-#     SVCMed.append((i, 'poly', polyacu/10))
-#     SVCMed.append((i, 'rbf', rbfacu/10))
-# SVCBest = (0.0, '', -1)
-# for svcmed in SVCMed:
-#     if svcmed[2] > SVCBest[2]:
-#         SVCBest = svcmed
-# print(SVCBest)
-#
-# Best = ((0, 0), 0)
-# Acur = []
-# for k in range(20):
-#     print(k)
-#     train_x, aux_x, train_y, aux_y = train_test_split(x, y, test_size=0.5)
-#     validation_x, test_x, validation_y, test_y = train_test_split(aux_x, aux_y, test_size=0.5)
-#     for i in range(3, 9):
-#         for j in np.arange(0.0001, 0.001, 0.0002):
-#             for l in range(100, 500, 20):
-#                 mlp = MLPClassifier(hidden_layer_sizes=(i, 15), random_state=1, learning_rate='constant', learning_rate_init=j, max_iter=l)
-#                 mlp.fit(train_x, train_y)
-#                 Acur.append(((i, j, l), accuracy(test_y, mlp.predict(test_x))))
-#
-#             # print(Acur)
-# # print(Acur)
-# Medias = []
-# for i in range(2, 10):
-#     for j in range(1, 10):
-#         for k in range(200, 300):
-#             Media = 0
-#             for acur in Acur:
-#                 if acur[0] == (i, j, k):
-#                     Media += acur[1]
-#             Medias.append(((i, j), Media/20))
-# # print(Medias)
-# for acur in Acur:
-#     if acur[1]>Best[1]:
-#         Best = acur
-# print(Best)
+Best = ((0, 0), 0)
+Acur = []
+for k in range(20):
+    print(k)
+    train_x, aux_x, train_y, aux_y = train_test_split(x, y, test_size=0.5)
+    validation_x, test_x, validation_y, test_y = train_test_split(aux_x, aux_y, test_size=0.5)
+    for i in range(1, 20):
+        for j in np.arange(0.005, 0.1, 0.005):
+            for l in range(100, 1000, 100):
+                mlp = MLPClassifier(hidden_layer_sizes=(i,), learning_rate_init=j, max_iter=l)
+                mlp.fit(train_x, train_y)
+                Acur.append(((i, j, l), accuracy(test_y, mlp.predict(test_x))))
+
+            # print(Acur)
+# print(Acur)
+Medias = []
+for i in range(2, 10):
+    for j in range(1, 10):
+        for k in range(200, 300):
+            Media = 0
+            for acur in Acur:
+                if acur[0] == (i, j, k):
+                    Media += acur[1]
+            Medias.append(((i, j), Media/10))
+# print(Medias)
+for acur in Acur:
+    if acur[1]>Best[1]:
+        Best = acur
+print(Best)
--- leve.py
+++ leve.py
@@ -24,7 +24,7 @@ for i in range(784):
 df1 = pd.DataFrame(columns=fields)
 df2 = pd.DataFrame(columns=fields2)
 
-for cla in range(1):
+for cla in range(10):
     print(cla)
     print("---------")
     mypath = "ct\\"+cla.__str__()+"\\"
@@ -37,67 +37,67 @@ for cla in range(1):
             print(F)
         if file != 'Thumbs.db':
             path = mypath + file
-
+            #
             image_file = Image.open(path)  # open colour image
             image_file = image_file.convert('1')  # convert image to black and white
             image_file = image_file.filter(ImageFilter.MaxFilter(3))
             image_file = image_file.filter(ImageFilter.MinFilter(3))
-            # image_file.save('result1.png')
-
+            # # image_file.save('result1.png')
+            #
             image_M = np.asanyarray(image_file)
-            start_x = -1
-            end_x = -1
-            start_y = -1
-            end_y = -1
-            for i in range(len(image_M)):
-                flag = False
-                for j in range(len(image_M)):
-                    if image_M[i][j]:
-                        # print(i, j)
-                        start_x = i
-                        flag = True
-                        break
-                if flag:
-                    break
-
-            for i in range(len(image_M) - 1, -1, -1):
-                flag = False
-                for j in range(len(image_M) - 1, -1, -1):
-                    if image_M[i][j]:
-                        # print(i, j)
-                        end_x = i
-                        flag = True
-                        break
-                if flag:
-                    break
-
-            for i in range(len(image_M)):
-                flag = False
-                for j in range(len(image_M)):
-                    if image_M[j][i]:
-                        # print(i, j)
-                        start_y = i
-                        flag = True
-                        break
-                if flag:
-                    break
-
-            for i in range(len(image_M) - 1, -1, -1):
-                flag = False
-                for j in range(len(image_M) - 1, -1, -1):
-                    if image_M[j][i]:
-                        # print(i, j)
-                        end_y = i
-                        flag = True
-                        break
-                if flag:
-                    break
+            # start_x = -1
+            # end_x = -1
+            # start_y = -1
+            # end_y = -1
+            # for i in range(len(image_M)):
+            #     flag = False
+            #     for j in range(len(image_M)):
+            #         if image_M[i][j]:
+            #             # print(i, j)
+            #             start_x = i
+            #             flag = True
+            #             break
+            #     if flag:
+            #         break
+            #
+            # for i in range(len(image_M) - 1, -1, -1):
+            #     flag = False
+            #     for j in range(len(image_M) - 1, -1, -1):
+            #         if image_M[i][j]:
+            #             # print(i, j)
+            #             end_x = i
+            #             flag = True
+            #             break
+            #     if flag:
+            #         break
+            #
+            # for i in range(len(image_M)):
+            #     flag = False
+            #     for j in range(len(image_M)):
+            #         if image_M[j][i]:
+            #             # print(i, j)
+            #             start_y = i
+            #             flag = True
+            #             break
+            #     if flag:
+            #         break
+            #
+            # for i in range(len(image_M) - 1, -1, -1):
+            #     flag = False
+            #     for j in range(len(image_M) - 1, -1, -1):
+            #         if image_M[j][i]:
+            #             # print(i, j)
+            #             end_y = i
+            #             flag = True
+            #             break
+            #     if flag:
+            #         break
 
             # print(start_x, start_y)
             # print(end_x, end_y)
             # print("numero isolado")
-            jump_x = (end_x - start_x) / 4
-            jump_y = (end_y - start_y) / 4
+            jump_x = 7
+            jump_y = 7
             # print(jump_x, jump_y)
             # print(round(jump_x), round(jump_y))
             black = 0
@@ -106,17 +106,16 @@ for cla in range(1):
             for i in range(16):
                 wb.append(0.0)
             wb.append(cla)
-
             for i in range(4):
                 for j in range(4):
-                    for k in range(start_x + round(i * jump_x), start_x + round((i + 1) * jump_x)):
-                        for l in range(start_y + round(j * jump_y), start_y + round((j + 1) * jump_y)):
+                    for k in range(round(i * jump_x), round((i + 1) * jump_x)):
+                        for l in range(round(j * jump_y), round((j + 1) * jump_y)):
                             if image_M[k][l]:
                                 white = white + 1
                             else:
                                 black = black + 1
                     if black == 0:
-                        wb[(i*4)+j] = white
+                        wb[(i*4)+j] = 1
                         # print(black, white, white)
                     else:
                         wb[(i*4)+j] = white / black
@@ -124,96 +123,8 @@ for cla in range(1):
                     black = 0
                     white = 0
             # classssssssssssssssss
+            df2e = pd.DataFrame([wb], columns=fields)
+            df2 = df2.append(df2e, ignore_index=True)
+            df2.to_csv('Base2.csv')
 
-            df1e = pd.DataFrame([wb], columns=fields)
-            df1 = df1.append(df1e, ignore_index=True)
-            df1.to_csv('ct\\Base1.csv')
-            # print(df1e)
-            # print(df1)
-            # print("--------------------------------------------------------")
-            # i*4+j
-            # entry = np.zeros(785)
-            # # setor vidno pela direito ao número
-            # for i in range(start_x, end_x+1):
-            #     for j in range(start_y-1):
-            #         entry[(i * 28) + j] = 1
-            # # setor de cima acima ao número
-            # for i in range(start_x-1):
-            #     for j in range(start_y, end_y+1):
-            #         entry[(i * 28) + j] = 8
-            # # setor vidno pela esquerda ao número
-            # for i in range(start_x, end_x+1):
-            #     for j in range(end_y+1, 28):
-            #         entry[(i * 28) + j] = 4
-            #     # setor de baixo abaixo do número
-            #     for i in range(end_x+1, 28):
-            #         for j in range(start_y, end_y+1):
-            #             entry[(i * 28) + j] = 2
-            #
-            #     # print(entry)
-            #     # print("------------------------------------------------------")
-            #     for i in range(start_x, end_x+1):
-            #         for j in range(start_y, end_y+1):
-            #             # para direita
-            #             k = j + 1
-            #             braked = False
-            #             if k < 28:
-            #                 while not image_M[i][k]:
-            #                     k += 1
-            #                     if k > end_y:
-            #                         braked = True
-            #                         break
-            #             else:
-            #                 braked = True
-            #             if not braked:
-            #                 entry[(i * 28) + j] += 1
-            #             # para baixo
-            #             k = i + 1
-            #             braked = False
-            #             if k < 28:
-            #                 while not image_M[k][j]:
-            #                     k += 1
-            #                     if k > end_x:
-            #                         braked = True
-            #                         break
-            #             else:
-            #                 braked = True
-            #             if not braked:
-            #                 entry[(i * 28) + j] += 8
-            #             # para esquerda
-            #             k = j - 1
-            #             braked = False
-            #             if k >= 0:
-            #                 while not image_M[i][k]:
-            #                     k -= 1
-            #                     if (k < start_y) or (k >= 0):
-            #                         braked = True
-            #                         break
-            #             else:
-            #                 braked = True
-            #             if not braked:
-            #                 entry[(i * 28) + j] += 4
-            #
-            #             # para cima
-            #             k = j - 1
-            #             braked = False
-            #             if k >= 0:
-            #                 while not image_M[k][j]:
-            #                     k -= 1
-            #                     if (k < end_x) or (k >= 0):
-            #                         braked = True
-            #                         break
-            #             if not braked:
-            #                 entry[(i * 28) + j] += 2
-            #
-            #             if image_M[i][j]:
-            #                 entry[(i * 28) + j] += 16
-            #     #classssssssssssssssss
-            #     entry[784] = cla
-            #     df2e = pd.DataFrame([entry.astype(int)], columns=fields2)
-            #     df2 = df2.append(df2e, ignore_index=True)
-
-
-df1.to_csv('Base1.csv')
-# df2.to_csv('Base2.csv')
 
